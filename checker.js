f321e32bda3a3be854815513bc5499a2f233d59b
Saicharan Neerugatti <105300049+Saicharan-Neerugatti@users.noreply.github.com>
2024-07-22 16:18:46 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -2,10 +2,12 @@ function batteryIsOk(temperature,  soc,  chargeRate) {
     if(temperature < 0 || temperature > 45) {
         console.log("Temperature is out of range!");
         return false;
-    } else if(soc < 20 || soc > 80) {
+    }
+    if(soc < 20 || soc > 80) {
         console.log("State of Charge is out of range!");
         return false;
-    } else if(chargeRate > 0.8) {
+    } 
+    if(chargeRate > 0.8) {
         console.log("Charge Rate is out of range!");
         return false;
     }
